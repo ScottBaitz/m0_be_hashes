ed8f4d28635161113ca6361e9dfcd60dd12f3724
Scott Baitz <Scott.W.Baitz@gmail.com>
2023-03-21 13:03:17 -0600

    Complete hashes

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,30 +1,34 @@
 
-Activity:
+# Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
 # file by entering the following command in your Terminal: 
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  apples: 23, 
+  grapes: 507, 
+  eggs: 48
+}
+
 
 # Write code that prints all of the 'keys' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+# p foods.keys
 
 # Write code that prints all of the 'values' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+# p foods.values
 
 # Write code that prints the value of the second food of the foods variable 
 # you created above:
-# YOUR CODE HERE
+# p foods[:grapes]
 
 # Write code that adds a food to the foods hash. 
 # Then, print the updated hash:
-# YOUR CODE HERE
-
+# p foods[:oreos] = 24
+# p foods.keys
 
 #-------------------
 # Part 2: Email
@@ -34,19 +38,28 @@ p zoo
 # Think about all the pieces of information associated with one single email in your inbox.
 # It has a sender, a subject, ...
 
+
+
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
-
+email = {
+  sender: "scott.w.baitz@gmail.com",
+  reciver: "velkar@gmail.com",
+  bcc: "",
+  subject: "DUDE: READ THIS",
+  content: "SYKE"
+}
 # Write code that prints your email hash to the terminal.
 
+# p email
 
 # Write code that prints all of the 'keys' of the email hash 
 # you created above:
-# YOUR CODE HERE
+# p email.keys
 
 # Write code that prints all of the 'values' of the email hash 
 # you created above:
-# YOUR CODE HERE
+# p email.values
 
 
 #-------------------
@@ -84,8 +97,8 @@ posts = [
   }
 ]
 
-p posts
-p posts[0]
+# p posts
+# p posts[0]
 
 
 # The code snippet above shows an Array with 2 elements. Each element in the Array is a 
@@ -97,3 +110,29 @@ p posts[0]
 # YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+
+email = [
+  {
+  sender: "scott.w.baitz@gmail.com",
+  reciver: "velkar@gmail.com",
+  bcc: "",
+  subject: "DUDE: READ THIS",
+  content: "SYKE"
+  },
+  {
+    sender: "email2@gmail.com",
+    reciver: "email1@gmail.com",
+    bcc: "",
+    subject: "FWRD: DUDE: READ THIS",
+    content: "HA HA"
+  },
+  {
+    sender: "email3.com",
+    reciver: "email4@gmail.com",
+    bcc: "",
+    subject: "FWRD: FWRD: DUDE: READ THIS",
+    content: "SYKE"
+  }
+]
+
+p email
